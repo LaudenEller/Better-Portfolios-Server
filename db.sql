a0b54f0bce19ee8e2a02d19544b8fd7f36ec8536
Lauden Eller <laudlauden@gmail.com>
2022-06-24 00:54:10 -0700

    Added more funds to the db

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-INSERT INTO 'invEStiGuideAPI_country' VALUES (null, "Global");
+INSERT INTO 'invEStiGuideAPI_issuer' VALUES (null, "MSCI", 11, "https://upload.wikimedia.org/wikipedia/commons/7/7e/MSCI_logo_2019.png");
 
-DELETE FROM invEStiGuideAPI_watchedsecurity
-WHERE fund_id = 6
+DELETE FROM invEStiGuideAPI_fund
+WHERE id > 0 AND id < 11
