e68be9cef5de86db93a80d2671611e67c79b41d4
Lauden Eller <laudlauden@gmail.com>
2022-07-01 09:40:18 -0700

    Updated fixtures

--- invEStiGuideAPI/models/asset_class.py
+++ invEStiGuideAPI/models/asset_class.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.forms import CharField
 
-class AssetClass(models.Model):
+class Asset_Class(models.Model):
    
    asset_class = models.CharField(max_length=200)
