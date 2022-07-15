57e78a1b8dd98dffaf105ce67781d01ce626a96b
Lauden Eller <laudlauden@gmail.com>
2022-07-14 19:53:36 -0800

    Added client to whitelist

--- invEStiGuide/settings.py
+++ invEStiGuide/settings.py
@@ -67,7 +67,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://betterportfoliosclient.herokuapp.com/'
 )
 
 ROOT_URLCONF = 'invEStiGuide.urls'
